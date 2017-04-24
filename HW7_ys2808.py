e69f4578cfdedea3bf9645b13fad80c867af2431
YUAN SHI <shiyuan315@outlook.com>
2017-04-24 15:53:35 -0400

    Add files via upload

--- HW7_ys2808.py
+++ HW7_ys2808.py
@@ -17,7 +17,7 @@ def getgeo(partID, records):
     geo = lon_lat.keys()
     return geo
 
-def getgeo(partID, records):
+def biketime(partID, records):
     if partID == 0:
         records.next()
     
@@ -72,10 +72,30 @@ if __name__ == "__main__":
     geo = rdd.mapPartitionsWithIndex(getgeo).collect()
 
     rdd = sc.textFile('/user/ys2808/citibike.csv')
-    starttime = rdd.mapPartitionsWithIndex(getgeo).collect()
+    starttime = rdd.mapPartitionsWithIndex(biketime).collect()
 
-
+    def findpair(partID, records):
+        if partID == 0:
+            records.next()
     
+        reader = csv.reader(records)
+    
+        from geopy.distance import great_circle
+
+        taxitime = []
+            for row in reader:
+                if (row[0].split(' ')[0] == '2015-02-01')&(row[4] != 'NULL') & (row[5] != 'NULL'):
+                    end = ( float(row[4]) , float(row[5]))
+                if great_circle(end, geo).miles <= 0.25:
+                    taxitime.append(row[0].split(' ')[1])
+        taxitime = map(lambda x: datetime.strptime(x[:7], '%H:%M:%S').time() ,taxitime)
+            count = 0
+        for i in taxitime:
+            for j in starttime:
+                if (i>= j[0]) & (i<= j[1]):
+                    count +=1
+        return [count]
+
     rdd = sc.textFile('/user/ys2808/yellow.csv.gz')
     count = rdd.mapPartitionsWithIndex(findpair)
     count.saveAsTextFile('/user/ys2808/hw7')
