739094f0536833767f4561873c073869de3cb031
YUAN SHI <shiyuan315@outlook.com>
2017-04-24 15:31:26 -0400

    Add files via upload

--- HW7_ys2808.py
+++ HW7_ys2808.py
@@ -1,73 +1,83 @@
+from __future__ import print_function
 
-#sc
-import csv
-from datetime import datetime, timedelta
+import sys
+
+from pyspark import SparkContext
+
 def getgeo(partID, records):
     if partID == 0:
         records.next()
-        
+    
     reader = csv.reader(records)
     lon_lat={}
 
     for row in reader:
         if (row[3].split(' ')[0] == '2015-02-01') & (row[6] == 'Greenwich Ave & 8 Ave') :
-            lon_lat[(row[7],row[8])] =1 
-    return lon_lat.keys()
-
-rdd = sc.textFile('citibike.csv')
-geo = rdd.mapPartitionsWithIndex(getgeo).collect()
-geo=geo[0]
-geo=(float(geo[0]), float(geo[1]))
+            lon_lat[(float(row[7]),float(row[8]))] =1
+    geo = lon_lat.keys()
+    return geo
 
 def getgeo(partID, records):
     if partID == 0:
         records.next()
-        
+    
     reader = csv.reader(records)
 
     starttime=[]
     for row in reader:
         if (row[3].split(' ')[0] == '2015-02-01') & (row[6] == 'Greenwich Ave & 8 Ave') :
             starttime.append(row[3].split(' ')[1])
-    return starttime 
-            
 
-rdd = sc.textFile('citibike.csv')
-biketime = rdd.mapPartitionsWithIndex(getgeo).collect()
+    starttime = map(lambda x: (datetime.strptime(x.split('+')[0], '%H:%M:%S') ,
+                           datetime.strptime(x.split('+')[0], '%H:%M:%S')+timedelta(minutes = 10)) , starttime)
+    starttime=map(lambda x: (x[0].time(),x[1].time()), starttime)
+    return starttime
+
 
 def findpair(partID, records):
     if partID == 0:
         records.next()
-        
+    
     reader = csv.reader(records)
     
     from geopy.distance import great_circle
 
-    taxitime []
+    taxitime = []
     for row in reader:
         if (row[0].split(' ')[0] == '2015-02-01')&(row[4] != 'NULL') & (row[5] != 'NULL'):
             end = ( float(row[4]) , float(row[5]))
             if great_circle(end, geo).miles <= 0.25:
                 taxitime.append(row[0].split(' ')[1])
-                
-    return taxitime
+    taxitime = map(lambda x: datetime.strptime(x[:7], '%H:%M:%S').time() ,taxitime)
+    count = 0
+    for i in taxitime:
+        for j in starttime:
+            if (i>= j[0]) & (i<= j[1]):
+                count +=1
+    return [count]
 
 
-rdd = sc.textFile('yellow.csv.gz')
-taxitime = rdd.mapPartitionsWithIndex(findpair).collect()
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: spark-submit sort <inputfile> <outputfile>", file=sys.stderr)
+        exit(-1)
 
 
-starttime = map(lambda x: (datetime.strptime(x.split('+')[0], '%H:%M:%S') ,
-                           datetime.strptime(x.split('+')[0], '%H:%M:%S')+timedelta(minutes = 10)) , biketime)
-starttime=map(lambda x: (x[0].time(),x[1].time()), starttime)
+    import csv
+    from datetime import datetime, timedelta
 
-taxitime = map(lambda x: datetime.strptime(x[:7], '%H:%M:%S').time() ,taxitime)
 
-count = 0
-for i in taxitime:
-    for j in starttime:
-        if (i>= j[0]) & (i<= j[1]):
-            count +=1
-            
-print count
+    rdd = sc.textFile('/user/ys2808/citibike.csv')
+    geo = rdd.mapPartitionsWithIndex(getgeo).collect()
 
+    rdd = sc.textFile('/user/ys2808/citibike.csv')
+    starttime = rdd.mapPartitionsWithIndex(getgeo).collect()
+
+
+    
+    rdd = sc.textFile('/user/ys2808/yellow.csv.gz')
+    taxitime = rdd.mapPartitionsWithIndex(findpair).collect()
+    taxitime.saveAsTextFile('/user/ys2808/hw7')
+    print taxitime
+
+    sc.stop()
