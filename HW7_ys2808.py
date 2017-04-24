4cc8d704aca5af773b7f2cf95a41b1f978c6fb9e
YUAN SHI <shiyuan315@outlook.com>
2017-04-24 16:00:47 -0400

    Add files via upload

--- HW7_ys2808.py
+++ HW7_ys2808.py
@@ -5,6 +5,7 @@ import sys
 from pyspark import SparkContext
 
 def getgeo(partID, records):
+    import csv
     if partID == 0:
         records.next()
     
@@ -32,29 +33,6 @@ def biketime(partID, records):
                            datetime.strptime(x.split('+')[0], '%H:%M:%S')+timedelta(minutes = 10)) , starttime)
     starttime=map(lambda x: (x[0].time(),x[1].time()), starttime)
     return starttime
-
-
-def findpair(partID, records):
-    if partID == 0:
-        records.next()
-    
-    reader = csv.reader(records)
-    
-    from geopy.distance import great_circle
-
-    taxitime = []
-    for row in reader:
-        if (row[0].split(' ')[0] == '2015-02-01')&(row[4] != 'NULL') & (row[5] != 'NULL'):
-            end = ( float(row[4]) , float(row[5]))
-            if great_circle(end, geo).miles <= 0.25:
-                taxitime.append(row[0].split(' ')[1])
-    taxitime = map(lambda x: datetime.strptime(x[:7], '%H:%M:%S').time() ,taxitime)
-    count = 0
-    for i in taxitime:
-        for j in starttime:
-            if (i>= j[0]) & (i<= j[1]):
-                count +=1
-    return [count]
 
 
 if __name__ == "__main__":
